d1c16e4d90c845684c1ade55ddcf40d9e4399e53
Ezio <eziosalapro@gmail.com>
2023-11-21 09:41:58 +0100

    modif helper ancien copy/paste

--- src/Utils/Helper.php
+++ src/Utils/Helper.php
@@ -4,8 +4,4 @@ namespace Oshop\Utils;
 
 class Helper
 {
-    public static function getStars($rate)
-    {
-        return str_repeat('<i class="fa fa-star"></i>', $rate) . str_repeat('<i class="fa fa-star-o"></i>', 5 - $rate);
-    }
 }
